20e624034f6a82cc1ee1bfb33ccd7445be56bc18
Opal <how2logout@gmail.com>
2025-01-06 20:31:06 +0900

    chore(config/autocmd): disable comment in new line

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -28,3 +28,14 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     })
   end,
 })
+
+-- Disable comment on new line
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    -- Disable comment on new line
+    vim.opt.formatoptions:remove({ "c", "r", "o" })
+  end,
+  group = vim.api.nvim_create_augroup("General", { clear = true }),
+  desc = "Disable New Line Comment",
+})
