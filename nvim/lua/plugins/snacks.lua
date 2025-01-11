a04b374c5eee9ac6b4a0ef66c3ae3a7174995de1
Opal <how2logout@gmail.com>
2025-01-11 22:26:39 +0900

    *

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -45,21 +45,75 @@ return {
         },
       },
       dashboard = {
-        formats = {
-          key = function(item)
-            return { { "[", hl = "special" }, { item.key, hl = "key" }, { "]", hl = "special" } }
-          end,
-        },
-        sections = {
-          { section = "terminal", cmd = "fortune -s | cowsay", hl = "header", padding = 1, indent = 8 },
-          { title = "MRU", padding = 1 },
-          { section = "recent_files", limit = 8, padding = 1 },
-          { title = "MRU ", file = vim.fn.fnamemodify(".", ":~"), padding = 1 },
-          { section = "recent_files", cwd = true, limit = 8, padding = 1 },
-          { title = "Sessions", padding = 1 },
-          { section = "projects", padding = 1 },
-          { title = "Bookmarks", padding = 1 },
-          { section = "keys" },
+        -- formats = {
+        --   key = function(item)
+        --     return { { "[", hl = "special" }, { item.key, hl = "key" }, { "]", hl = "special" } }
+        --   end,
+        -- },
+        -- sections = {
+        --   { section = "terminal", cmd = "fortune -s | cowsay", hl = "header", padding = 1, indent = 8 },
+        --   { title = "MRU", padding = 1 },
+        --   { section = "recent_files", limit = 8, padding = 1 },
+        --   { title = "MRU ", file = vim.fn.fnamemodify(".", ":~"), padding = 1 },
+        --   { section = "recent_files", cwd = true, limit = 8, padding = 1 },
+        --   { title = "Sessions", padding = 1 },
+        --   { section = "projects", padding = 1 },
+        --   { title = "Bookmarks", padding = 1 },
+        --   { section = "keys" },
+        -- },
+        {
+          sections = {
+            { section = "header" },
+            {
+              pane = 2,
+              section = "terminal",
+              cmd = "colorscript -e square",
+              height = 5,
+              padding = 1,
+            },
+            { section = "keys", gap = 1, padding = 1 },
+            function()
+              local in_git = Snacks.git.get_root() ~= nil
+              local cmds = {
+                {
+                  title = "Notifications",
+                  cmd = "gh notify -s -a -n5",
+                  icon = " ",
+                  height = 5,
+                  enabled = true,
+                },
+                {
+                  title = "Open Issues",
+                  cmd = "gh issue list -L 3",
+                  icon = " ",
+                  height = 7,
+                },
+                {
+                  icon = " ",
+                  title = "Open PRs",
+                  cmd = "gh pr list -L 3",
+                  height = 7,
+                },
+                {
+                  icon = " ",
+                  title = "Git Status",
+                  cmd = "hub --no-pager diff --stat -B -M -C",
+                  height = 10,
+                },
+              }
+              return vim.tbl_map(function(cmd)
+                return vim.tbl_extend("force", {
+                  pane = 2,
+                  section = "terminal",
+                  enabled = in_git,
+                  padding = 1,
+                  ttl = 5 * 60,
+                  indent = 3,
+                }, cmd)
+              end, cmds)
+            end,
+            { section = "startup" },
+          },
         },
       },
     },
