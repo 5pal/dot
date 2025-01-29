1a382ce68a91e356f15f1fa23fc1aabf99bfdeeb
Opal <how2logout@gmail.com>
2025-01-29 23:30:11 +0900

    feat: *

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,17 +6,7 @@ _G.dd = function(...)
   require("util.debug").dump(...)
 end
 vim.print = _G.dd
--- I'm using Snacks.nvim
--- _G.dd = function(...)
---   require("snakcs.debug").inspect(...)
--- end
--- _G.bt = function(...)
---   require("snakcs.debug").backtrace()
--- end
--- _G.p = function(...)
---   require("snacks.debug").profile(...)
--- end
--- vim.print = _G.dd
+
 vim.filetype.add({
   extension = {
     vert = "glsl",
