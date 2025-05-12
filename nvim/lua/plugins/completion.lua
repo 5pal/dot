6bd18ed3f826bcb0c7857c99476d3c13039770c4
Opal <how2logout@gmail.com>
2025-05-12 18:21:27 +0900

    *

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -31,8 +31,29 @@ return {
       -- Integrate nvim-autopairs with cmp
       -- cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
+      local options = {
+        history = true,
+        update_events = "InsertLeave",
+        enable_autosnippets = true,
+        region_check_events = "CursorHold,InsertLeave",
+        delete_check_events = "TextChanged,InsertEnter",
+        store_selection_keys = "<Tab>",
+      }
+
+      luasnip.config.set_config(options)
+      vim.api.nvim_create_autocmd("InsertLeave", {
+        callback = function()
+          if
+            require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+            and not require("luasnip").session.jump_active
+          then
+            require("luasnip").unlink_current()
+          end
+        end,
+      })
+
       -- Load snippets
-      require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } })
 
       cmp.setup({
         snippet = {
