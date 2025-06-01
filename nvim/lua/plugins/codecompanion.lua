b03c85d4f4b78028bca8509c0a8b60cf231afea4
Opal <how2logout@gmail.com>
2025-06-01 23:04:29 +0900

    plugin bumps

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -2,27 +2,36 @@
 return {
     {
         'olimorris/codecompanion.nvim',
-        cmd = {
-            'CodeCompanion',
-            'CodeCompanionChat',
-            'CodeCompanionToggle',
-            'CodeCompanionActions',
+        cmd = 'CodeCompanion',
+        keys = {
+            { '<leader>at', '<cmd>CodeCompanionChat Toggle<cr>', desc = 'Toggle CodeCompanion chat' },
+            { '<leader>aa', '<cmd>CodeCompanionChat Add<cr>', desc = 'Add to CodeCompanion chat', mode = 'x' },
         },
-        opts = {
-            strategies = {
-                inline = {
-                    keymaps = {
-                        accept_change = {
-                            modes = { n = '<leader>ca' },
-                            description = 'Accept the suggested change',
-                        },
-                        reject_change = {
-                            modes = { n = '<leader>cr' },
-                            description = 'Reject the suggested change',
+        opts = function()
+            local config = require('codecompanion.config').config
+
+            local diff_opts = config.display.diff.opts
+            table.insert(diff_opts, 'context:99') -- Setting the context to a very large number disables folding.
+
+            return {
+                strategies = {
+                    inline = {
+                        keymaps = {
+                            accept_change = {
+                                modes = { n = '<leader>ay' },
+                                description = 'Accept the suggested change',
+                            },
+                            reject_change = {
+                                modes = { n = '<leader>an' },
+                                description = 'Reject the suggested change',
+                            },
                         },
                     },
                 },
-            },
-        },
+                display = {
+                    diff = { opts = diff_opts },
+                },
+            }
+        end,
     },
 }
