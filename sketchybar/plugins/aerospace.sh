6a4596b267d9f89f6d1143280b9bb63347cd8852
Opal <how2logout@gmail.com>
2025-08-15 23:37:33 +0900

    plugin bumps

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -4,7 +4,7 @@
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 source "$CONFIG_DIR/plugins/colors.sh"
 
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+if [ "$1" = "$FOCUSED_WORKSPACE" ] || [ "$1" = "$(aerospace list-workspaces --focused)" ]; then
   sketchybar --set $NAME background.color=$HIGHLIGHT_BACKGROUND label.color=$TEXT_WHITE icon.color=$TEXT_WHITE background.border_color=$TEXT_GREY
 else
   sketchybar --set $NAME background.color=$TRANSPARENT label.color=$TEXT_GREY icon.color=$TEXT_GREY background.border_color=$TEXT_WHITE
